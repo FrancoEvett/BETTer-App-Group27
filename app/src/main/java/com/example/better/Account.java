aea00c0baa24ffcfd565043fb65bda371347a50b
FrancoEvett <francoevett@hotmail.co.uk>
2020-01-31 09:46:45 +0000

    Again Updated my Version

--- app/src/main/java/com/example/better/Account.java
+++ app/src/main/java/com/example/better/Account.java
@@ -14,7 +14,6 @@ public class Account {
     //hold information on labs
     public Dictionary<String, String> LabNumbers;
 
-
     Account(int userStudentID, String userName, String userEmail, String userPassword){
         //initalise variables to class from contrustor
         StudentID = userStudentID;
@@ -22,37 +21,6 @@ public class Account {
         Email = userEmail;
         Password = userPassword;
     }
-
-    //---------------------------------------------------------------------------------------------------
-    //---------------------------------------------------------------------------------------------------
-    //--------------------------------------EXTERNALLY CALLED METHODS-------------------------------------
-    //call this to setup the users labs
-    public void SetLabs(String CS2001, String CS2002, String CS2003, String CS2004, String CS2005){
-        //first clear the old dictionary
-        LabNumbers = null;
-
-        //now add the new lab values
-        LabNumbers.put("CS2001", CS2001);
-        LabNumbers.put("CS2002", CS2002);
-        LabNumbers.put("CS2003", CS2003);
-        LabNumbers.put("CS2004", CS2004);
-        LabNumbers.put("CS2005", CS2005);
-    }
-
-    //call this to get the users lab number for specific module
-    public String GetLabNumber(String ModuleCode){
-        return LabNumbers.get(ModuleCode);
-    }
-
-    //call this to get a dictionary of lab codes and the user lab number
-    public Dictionary<String, String> GetLabDictionary(){
-        return LabNumbers;
-    }
-
-    //------------------------------------EXTERNALLY CALLED METHODS--------------------------------------
-    //---------------------------------------------------------------------------------------------------
-    //---------------------------------------------------------------------------------------------------
-
 }
 
 
