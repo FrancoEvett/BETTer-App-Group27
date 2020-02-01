9b7663524103aed8dda7c684d509717d49e85b46
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2020-02-01 14:32:41 +0000

    Added registration database backend

--- app/src/main/java/com/example/better/DatabaseControllerUser.java
+++ app/src/main/java/com/example/better/DatabaseControllerUser.java
@@ -25,20 +25,16 @@ public class DatabaseControllerUser extends AsyncTask<String, Void, String> {
     String data;
     UserAccountControll userAccountControll = new UserAccountControll();
 
-DatabaseControllerUser(Context ctx){
-    this.context = ctx;
+    DatabaseControllerUser(Context ctx) {
+        this.context = ctx;
 
-}
-
-
-
+    }
 
     @Override
-    protected String doInBackground(String... voids)
-    {
+    protected String doInBackground(String... voids) {
         String type = voids[0];
 
-        if(type.equals("login")){
+        if (type.equals("login")) {
             String studentID = voids[1];
             //???  String password = voids[2];
             String link = "https://brunelbetterapp.000webhostapp.com/userLogin.php";
@@ -50,7 +46,7 @@ DatabaseControllerUser(Context ctx){
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
                 OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
                 String post_data = URLEncoder.encode("studentID", "UTF-8") + "=" + URLEncoder.encode(studentID, "UTF-8");
                 bufferedWriter.write(post_data);
                 bufferedWriter.flush();
@@ -62,7 +58,7 @@ DatabaseControllerUser(Context ctx){
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
 
                 String line;
-                while ((line = bufferedReader.readLine()) != null){
+                while ((line = bufferedReader.readLine()) != null) {
                     data += line;
                 }
 
@@ -74,7 +70,7 @@ DatabaseControllerUser(Context ctx){
 
                 //Parsing the JSON data into single variables by passing the data string
                 JSONArray jsonArray = new JSONArray(data);
-                for(int i=0; i <jsonArray.length(); i++ ){
+                for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject jsonObject = (JSONObject) jsonArray.get(i);
 
                     String student_ID = jsonObject.get("studentID").toString();
@@ -84,10 +80,8 @@ DatabaseControllerUser(Context ctx){
 
 
                     //call the method ReceiveUserInfo to send data
-                    userAccountControll.ReceiveUserInfo(studentID,userName,userEmail,userPassword);
+                    userAccountControll.ReceiveUserInfo(studentID, userName, userEmail, userPassword);
                 }
-
-
 
 
             } catch (MalformedURLException e) {
@@ -99,12 +93,73 @@ DatabaseControllerUser(Context ctx){
             }
 
 
+        } else if (type.equals("register")) {
+            String register_url = "";
+
+            try {
+                String studentID = voids[1];
+                String userName = voids[2];
+                String userEmail = voids[3];
+                String userPassword = voids[4];
+
+                URL url = new URL(register_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("studentID", "UTF-8") + "=" + URLEncoder.encode(studentID, "UTF-8") + "&"
+                        + URLEncoder.encode("userName", "UTF-8") + "=" + URLEncoder.encode(userName, "UTF-8") + "&"
+                        + URLEncoder.encode("userEmail", "UTF-8") + "=" + URLEncoder.encode(userEmail, "UTF-8") + "&"
+                        + URLEncoder.encode("userPassword", "UTF-8") + "=" + URLEncoder.encode(userPassword, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                //Reading the results
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                //Closing the connections
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+
+                //sending true or false or any info
+                if(result.equals("User Exist")){
+                    return "User Exit";
+                }
+                else if(result.equals("User Added")){
+                    return "User Added";
+                }
+                else if(result.equals("Error")){
+                    return "Error";
+                }
+                return result;
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
 
-
-
-
         return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+
+            //send the info
     }
 }
