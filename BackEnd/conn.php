9dcbc722328edfd8fc874dc529dec32d2851438c
Pruthvi Lalji <pruthvi_lalji@hotmail.co.uk>
2020-03-18 20:20:00 +0000

    All BackEnd Files

---
+++ BackEnd/conn.php
@@ -0,0 +1,11 @@
+<?php 
+
+//Making basic connection to the database
+$db_name = "id11747915_timetable";
+$mysql_username = "id11747915_rootbrunel";
+$mysql_pass = "Pruthvidiu123";
+$server_name = "localhost";
+
+//Making a connection
+$conn = mysqli_connect($server_name, $mysql_username, $mysql_pass, $db_name);
+?>
