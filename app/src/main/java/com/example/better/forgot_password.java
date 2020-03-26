af0c2f58f39f65d94eb38ae5a42de8826814f33a
azouhir <1835227@brunel.ac.uk>
2020-03-26 21:50:29 +0000

    I tested the code different times with my email without luck, I believe the problem relies in my laptop as the antivirus is interfering if anyone could try it out and let me know it would be great

--- app/src/main/java/com/example/better/forgot_password.java
+++ app/src/main/java/com/example/better/forgot_password.java
@@ -30,6 +30,40 @@ String email = "BETTer@brunel.ac.uk";
         EditText em = findViewById(R.id.email);
         TextView mex = findViewById(R.id.textView12);
         String usermail = em.getText().toString();
+        if (!usermail.equals("") == true) {
+            try {
+                GMailSender sender = new GMailSender("better.group27@gmail.com", "BETTer_27");
+                sender.sendMail("This is Subject",
+                        "Your password is: ",
+                        "better.group27@gmail.com",
+                        usermail);
+                mex.setText("Thanks! your request has been received, please check your email for the next step");
+            } catch (Exception e) {
+                Log.e("SendMail", e.getMessage(), e);
+                mex.setText("The email is not recognised");
+            }
+        }
+        else {
+            mex.setText("Enter your email please!");
+        }
+
+
+//            Intent i = new Intent(Intent.ACTION_SEND);
+//            i.setType("message/rfc822");
+//            i.putExtra(Intent.EXTRA_EMAIL  , new String[]{usermail});
+//            i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+//            i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+//            try {
+//                startActivity(Intent.createChooser(i, "Your password is FUCK OFF"));
+//            } catch (android.content.ActivityNotFoundException ex) {
+//                mex.setText("This email is not registered in our database!");
+//            }
+ //       }
+//        else {
+//            //mex.setText("Email not recognised, please try again");
+//            popup("Error", "Email not recognised, please try again" );
+//            // Invalid email Error Message
+//        }
     }
 
     public void back (View view){
